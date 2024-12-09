c9c6ba25c661ee43826caf14557897aa24edbee6
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-12-09 16:04:44 -0300

    Customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  return <Button>Hello</Button>
+  return (
+    <div className="flex gap-2">
+      <Input />
+      <Button variant="primary">Primary</Button>
+      <Button variant="primary" disabled>
+        Primary
+      </Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
+    </div>
+  );
 }
