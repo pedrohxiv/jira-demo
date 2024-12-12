a846deac4537e8515a119e766a8f2036d1a8fadc
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-12-12 16:30:02 -0300

    Setting up appwrite & database

--- src/app/api/[[...route]]/auth.ts
+++ src/app/api/[[...route]]/auth.ts
@@ -1,22 +1,53 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { deleteCookie, setCookie } from "hono/cookie";
+import { ID } from "node-appwrite";
 
+import { AUTH_COOKIE } from "@/constants";
+import { createAdminClient } from "@/lib/appwrite";
 import { signInSchema, signUpSchema } from "@/schemas/auth";
 
 const app = new Hono()
-  .post("/sign-in", zValidator("json", signInSchema), (c) => {
+  .post("/sign-in", zValidator("json", signInSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
-    console.log({ email, password });
+    const { account } = await createAdminClient();
 
-    return c.json({ email, password });
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ success: true });
   })
-  .post("/sign-up", zValidator("json", signUpSchema), (c) => {
+  .post("/sign-up", zValidator("json", signUpSchema), async (c) => {
     const { name, email, password } = c.req.valid("json");
 
-    console.log({ name, email, password });
+    const { account } = await createAdminClient();
 
-    return c.json({ name, email, password });
+    await account.create(ID.unique(), email, password, name);
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ success: true });
+  })
+  .post("/sign-out", (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+
+    return c.json({ success: true });
   });
 
 export default app;
